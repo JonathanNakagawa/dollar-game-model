5ce5e69f6db7456ae39ebb9db5ede126f124b09a
Jonathan Nakagawa <jyn2@duke.edu>
2018-09-02 16:16:42 -0400

    completed functions to simulate a single game being played

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+numActive = 0
 
 class Player:
     def __init__(self, pos):
@@ -7,38 +8,67 @@ class Player:
         self.value = 3
         self.pos = pos
 
-    def retState(self):
+    def getState(self):
         return self.active
 
-    def retValue(self):
+    def getValue(self):
         return self.value
 
-    def retPos(self):
+    def getPos(self):
         return self.pos
 
     def changeValue(self, change):
+        global numActive
         new = self.value + change
         if(new <= 0):
-            self.value = 0
-            self.state = False
-        else:
-            self.value = new
-            self.state = True
+            self.active = False
+            numActive -= 1
+        elif self.value == 0 & new > 0:
+            self.active = True
+            numActive += 1
+        self.value = new
+
 
 
 def genTable(num):
     table = []
-    for x in range(0,num):
+    for x in range(0, num):
         temp = Player(x)
         table.append(temp)
-        print("test")
     return table
 
 
 def playTurn(table, pos):
     curPlayer = table[pos]
-    if not curPlayer.retState():
+    tableSize = len(table)
+    if not curPlayer.getState():
         return
+    rollCount = rollNum(curPlayer)
+    for i in range(0, rollCount):
+        result = rollDie()
+        if result == 0:
+            table[getLeftPos(pos, tableSize)].changeValue(1)
+        elif result == 1:
+            table[getRightPos(pos, tableSize)].changeValue(1)
+        elif result == 2:
+            curPlayer.changeValue(-1)
+
+
+def rollNum(curPlayer):
+    if curPlayer.getValue() < 3:
+        return curPlayer.getValue()
+    return 3
+
+def getLeftPos(curPos, tableSize):
+    if curPos == 0:
+        return tableSize - 1
+    return curPos - 1
+
+
+def getRightPos(curPos, tableSize):
+    if curPos == tableSize - 1:
+        return 0
+    return curPos + 1
 
 
 def rollDie():
@@ -54,18 +84,16 @@ def rollDie():
 
 
 def main():
-    playerCount = input("How many players? ")
-    playerCount = int(playerCount)
+    global numActive
+    playerCount = 8
     table = genTable(playerCount)
     numActive = playerCount
     pos = 0
     while numActive != 1:
         playTurn(table, pos)
-        if pos == playerCount - 1:
-            pos = 0
-        else:
-            pos += 1
-
+        pos = getRightPos(pos, playerCount)
+        print(numActive)
+    print()
 
 if __name__ == "__main__":
     main()
